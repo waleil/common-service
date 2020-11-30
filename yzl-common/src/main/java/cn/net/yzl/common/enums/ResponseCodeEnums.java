d3cdcc3b925fcd6aff3278ac34b6ab443e2af390
zhangyong <z.hang20080413@163.com>
2020-11-30 10:13:14 +0800

    修改响应 的码的命名规则 及一些其他的公共的响应码

--- yzl-common/src/main/java/cn/net/yzl/common/enums/ResponseCodeEnums.java
+++ yzl-common/src/main/java/cn/net/yzl/common/enums/ResponseCodeEnums.java
@@ -9,19 +9,59 @@ import lombok.Getter;
  */
 @Getter
 public enum ResponseCodeEnums {
-
 	SUCCESS_CODE(200, "成功","成功"),
 	SUCCESS_NODATA_CODE(201, "返回数据为空","成功，无数据"),
-	PARAMS_TYPE_ERROR_CODE(20000, "参数类型不匹配", "参数类型不匹配"),
-	API_ERROR_CODE(20001, "接口调用错误", "接口调用错误"),
-	SYSTEM_ERROR_CODE(20002, "系统错误", "系统错误"),
-	PARAMS_ERROR_CODE(20003, "参数验证错误", "参数验证错误"),
-	LOGIN_ERROR_CODE(20004, "登录错误", "登录错误"),
-	AUTHOR_ERROR_CODE(20005, "无访问权限", "无访问权限"),
-	VALIDATE_ERROR_CODE(20006, "规则校验不通过", "规则校验不通过"),
-	REPEAT_ERROR_CODE(30001, "请勿重复提交", "重复提交"),
-	BIZ_ERROR_CODE(30002, "业务异常", "业务逻辑错误"),
-	SIGN_ERROR_CODE(50001, "签名错误", "签名错误"),
+	// 用5位标识错误代码 12100 1:服务(例如:1所在位置表示crm,ehr,dmc,bi等项目,1现在表示公共响应码)
+	// 						 12:表示模块
+	//						 100:表示code
+	// 100开头   表示标识公共的响应码   例如:  参数错误,参数验证错误...
+	// 110开头   表示认证服务的
+	// 枚举名称规则  code英文描述+ERROR+CODE
+	PARAMS_TYPE_ERROR_CODE(10000, "参数类型不匹配", "参数类型不匹配"),
+	API_ERROR_CODE(10001, "接口调用错误", "接口调用错误"),
+	SYSTEM_ERROR_CODE(10002, "系统错误", "系统错误"),
+	PARAMS_EMPTY_ERROR_CODE(10003, "参数为空","参数为空"),
+	PARAMS_ERROR_CODE(10004, "参数验证错误", "参数验证错误"),
+	SERVICE_ERROR_CODE(10005, "服务异常","服务异常"),
+	TOKEN_INVALID_ERROR_CODE(10006, "token值无效","token值无效"),
+	TIMESTAMP_ERROR_CODE(10007, "时间戳错误","时间戳错误"),
+	JSON_PARAM_ERROR_CODE(10008,"参数JSON解析错误","参数JSON解析错误"),
+	AUTHOR_ERROR_CODE(10009, "无访问权限", "无访问权限"),
+	VALIDATE_ERROR_CODE(10010, "规则校验不通过", "规则校验不通过"),
+	REPEAT_ERROR_CODE(10011, "请勿重复提交", "重复提交"),
+	BIZ_ERROR_CODE(10012, "业务异常", "业务逻辑错误"),
+	SIGN_ERROR_CODE(10013, "签名错误", "签名错误"),
+	// 2开头表示 crm, 3:开头表示 ehr 4:开头表示dmc 5:开头:bi
+	// 211 开头表示 员工服务
+	// 212 开头表示 顾客服务
+	// 213 开头表示 商品服务
+	// 214 开头表示 媒介服务
+	// 215 开头表示 订单服务
+	// 216 开头表示 物流服务
+	// 217 开头表示 优惠服务
+	// 218 开头表示 员工商城服务
+	// 219 开头表示 工单服务
+	// 220 开头表示 质检服务
+	// 221 开头表示 仓储中心
+	// 218 开头表示 营销活动
+	// 220 开头表示 结算中心
+
+	// 3:开头表示 ehr
+	// 311  ......
+
+	// 4开头表示 dmc
+	// 411  ......
+
+	// 5开头表示 bi
+	// 511 .....
+
+
+
+
+
+
+
+
 
 
 	;
