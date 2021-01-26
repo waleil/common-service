dd3d2df6d95d5565863062667fc5fcd64e66dec9
renjudong <renjudong@yuzhilin.net.cn>
2021-01-26 14:50:11 +0800

    代码微调

--- yzl-common-zt/src/main/java/cn/net/yzl/zt/controller/RegionProvinceController.java
+++ yzl-common-zt/src/main/java/cn/net/yzl/zt/controller/RegionProvinceController.java
@@ -1,8 +1,6 @@
 package cn.net.yzl.zt.controller;
 
 import cn.net.yzl.common.entity.ComResponse;
-import cn.net.yzl.common.entity.Page;
-import cn.net.yzl.common.util.AssemblerResultUtil;
 import cn.net.yzl.zt.entity.RegionProvince;
 import cn.net.yzl.zt.service.RegionProvinceService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/regionProvince")
@@ -26,9 +26,10 @@ public class RegionProvinceController {
     //@OperateLog(operModule = "大区:查询大区列表", businessType = BusinessType.SELECT)
     @GetMapping("/getRegionProvinceList")
     public ComResponse getRegionProvinceList(){
+
         List<RegionProvince> regionProvinceList = regionProvinceService.getRegionProvinceList();
-        Page<RegionProvince> commonPageVO = AssemblerResultUtil.resultAssemblerSingleList(regionProvinceList);
-        return ComResponse.success(commonPageVO);
+        Map<String, List<RegionProvince>> regionProvinceListMap = regionProvinceList.stream().collect(Collectors.groupingBy(RegionProvince::getRegionName));
+        return ComResponse.success(regionProvinceListMap);
     }
 
 
