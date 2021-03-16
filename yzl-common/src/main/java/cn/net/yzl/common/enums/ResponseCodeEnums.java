4fbefb68b0400bbe898a7ee0787f9dfbe09e3633
tangwenjie <tangwenjie@yuzhilin.net.cn>
2021-03-16 19:32:02 +0800

    增加error_code,增加数字转汉字工具类

--- yzl-common/src/main/java/cn/net/yzl/common/enums/ResponseCodeEnums.java
+++ yzl-common/src/main/java/cn/net/yzl/common/enums/ResponseCodeEnums.java
@@ -83,7 +83,7 @@ public enum ResponseCodeEnums {
     STAFF_CONTART_EXPIRED_ERROR(21133,"过期合同信息无法录入！","过期合同信息无法录入！"),
     STAFF_CONTART_TIME_CROSS_ERROR(21134,"新增合同时间区间错误!","新增合同时间区间错误!"),
     STAFF_CONTART_INACTIVE_CROSS_ERROR(21135,"待用合同已存在,不可重复添加!","待用合同已存在,不可重复添加!"),
-    STAFF_AUTO_ENTER_POST_ERROR(21136,"员工自动入岗出错！","员工自动入岗出错！"),
+    STAFF_AUTO_ENTER_POST_ERROR(21136,"员工入岗出错！","员工入岗出错！"),
     // 212 开头表示 顾客服务
     // 213 开头表示 商品服务
     // 214 开头表示 媒介服务
