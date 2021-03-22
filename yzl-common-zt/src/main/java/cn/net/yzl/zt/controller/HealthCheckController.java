4661ddf715e46ea8550363be090a9e8659ddc5dd
renjudong <renjudong@yuzhilin.net.cn>
2021-03-22 18:48:28 +0800

    添加修复缓存方法

--- yzl-common-zt/src/main/java/cn/net/yzl/zt/controller/HealthCheckController.java
+++ yzl-common-zt/src/main/java/cn/net/yzl/zt/controller/HealthCheckController.java
@@ -1,12 +1,20 @@
 package cn.net.yzl.zt.controller;
 
+import cn.net.yzl.zt.config.redis.RedisUtil;
+import cn.net.yzl.zt.model.constant.RedisConstant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/healthCheck")
 public class HealthCheckController {
+
+    @Autowired
+    private RedisUtil redisUtil;
 
     /**
      * 健康检查
@@ -17,4 +25,22 @@ public class HealthCheckController {
         return "OK";
     }
 
+
+    /**
+     * 修复缓存
+     * @return
+     */
+    @GetMapping("/repairRedisInfo")
+    public String repairRedisInfo(@RequestParam String key,@RequestParam(required = false) String item) {
+        if(StringUtils.hasText(item)) {
+            String keys = (String) redisUtil.hget(key, item);
+            if (StringUtils.hasText(keys)) {
+                redisUtil.hdel(key,item);
+            }
+        }
+        if(StringUtils.hasText(key)){
+            redisUtil.del(key);
+        }
+        return "OK";
+    }
 }
